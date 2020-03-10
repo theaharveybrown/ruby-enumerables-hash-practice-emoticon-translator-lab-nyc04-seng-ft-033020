b9b12a4097d5ef67510c423327237772fa78aaeb
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-10 17:56:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,7 @@ require "yaml"
 def load_library(file_location)
   # code goes here
   emojis = YAML.load_file(file_location)
+  emojis.map{|k, v| k}
   
   emojis.map{|k| dictionary[:get_meaning] = k}
   puts dictionary
