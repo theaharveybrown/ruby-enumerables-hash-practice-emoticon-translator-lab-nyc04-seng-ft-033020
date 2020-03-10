ab4a5e519828a5b0116b1ff2ade1082c5e07393a
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-10 18:06:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(file_location)
   
   emojis.each do |k, v|
     puts k 
-    puts v 
+    # puts v 
   end 
   # translator[:get_meaning] = emojis.map{|k,v| k}
   # translator[:get_emoticon] = emojis.map{|k,v| v}
